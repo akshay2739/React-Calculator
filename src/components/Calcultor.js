81cb6fd13524fd7e8624964baf8168bf0c3fe4ea
akshay2739 <akshay2739@gmail.com>
2020-04-15 21:09:11 +0530

    Installed Dependencies

---
+++ src/components/Calcultor.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Calcultor extends Component {
+    render() {
+        return (
+            <div>
+                Calcultor
+            </div>
+        )
+    }
+}
